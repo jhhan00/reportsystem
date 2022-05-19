d61c59ab24c72f3acaa23bb7061204a13dfc4256
jhhan00 <jhhan009@gmail.com>
2022-05-19 23:53:35 +0900

    login success handler 수정

--- src/main/java/com/example/reportsystem/handler/MyLoginSuccessHandler.java
+++ src/main/java/com/example/reportsystem/handler/MyLoginSuccessHandler.java
@@ -3,11 +3,13 @@ package com.example.reportsystem.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
@@ -18,6 +20,12 @@ public class MyLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         logger.info("login success");
+
+        HttpSession session = request.getSession();
+        if(session != null) {
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);    // 이전에 로그인 실패한 이력이 있다면 session에서 실패 이력 제거
+        }
+
         response.sendRedirect("/");
     }
 }
