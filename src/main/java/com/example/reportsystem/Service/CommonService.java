616997694b37563d6803304a2274065fb7c75b09
jhhan00 <jhhan009@gmail.com>
2022-06-03 23:24:50 +0900

    커먼서비스: 지금은 안 쓰지만 나중에는 쓸 것 같아서 남겨놓음

--- src/main/java/com/example/reportsystem/Service/CommonService.java
+++ src/main/java/com/example/reportsystem/Service/CommonService.java
@@ -1,4 +1,4 @@
-package com.example.reportsystem.common;
+package com.example.reportsystem.Service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +8,9 @@ import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -20,6 +23,7 @@ public class CommonService {
 
     public boolean isAuthenticated(HttpServletRequest request, Authentication auth) {
         HttpSession httpSession = request.getSession();
+        logger.info("httpSession.getId = " + httpSession.getId());
         Enumeration<String> keys = httpSession.getAttributeNames();
         while(keys.hasMoreElements()) {
             String key = keys.nextElement();
@@ -27,21 +31,34 @@ public class CommonService {
             if(key.equals("SPRING_SECURITY_CONTEXT")) {
                 SecurityContextImpl attribute = (SecurityContextImpl)httpSession.getAttribute(key);
                 UsernamePasswordAuthenticationToken upat = (UsernamePasswordAuthenticationToken)attribute.getAuthentication();
-                logger.info("upat.getPrincipal   = " + upat.getPrincipal());
+                logger.info("  upat.getPrincipal   = " + upat.getPrincipal());
 //                logger.info("upat.getName        = " + upat.getName());
 //                logger.info("upat.getCredentials = " + upat.getCredentials());
 //                logger.info("upat.getAuthorities = " + upat.getAuthorities());
 //                logger.info("upat.getDetails     = " + upat.getDetails());
 
                 WebAuthenticationDetails wad = (WebAuthenticationDetails)upat.getDetails();
-                logger.info("wad.getSessionId = " + wad.getSessionId());
+                logger.info("  wad.getSessionId = " + wad.getSessionId());
 
                 User user = (User)upat.getPrincipal();
-                logger.info("user.getUsername    = " + user.getUsername());
-                logger.info("user.getPassword    = " + user.getPassword());
-                logger.info("user.getAuthorities = " + user.getAuthorities());
+                logger.info("  user.getUsername    = " + user.getUsername());
+//                logger.info("user.getPassword    = " + user.getPassword());
+                logger.info("  user.getAuthorities = " + user.getAuthorities());
             }
         }
+
+        logger.info("=============================");
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+        if(attributes != null) {
+            HttpSession session = attributes.getRequest().getSession(false);
+            Enumeration<String> names = session.getAttributeNames();
+            while(names.hasMoreElements()) {
+                String key = names.nextElement();
+                logger.info(key + " => " + httpSession.getAttribute(key));
+            }
+        }
+
         logger.info("auth = " + auth);
 
         return auth != null;
