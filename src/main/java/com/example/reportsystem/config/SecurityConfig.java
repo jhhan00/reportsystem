d89734ff6a9be138a05129b6799331589fc87956
jhhan00 <jhhan009@gmail.com>
2022-06-14 23:47:31 +0900

    h2 db와 Spring Security를 함께 사용하는 경우 SecurityConfig에 설정 추가 필수

--- src/main/java/com/example/reportsystem/config/SecurityConfig.java
+++ src/main/java/com/example/reportsystem/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.reportsystem.handler.MyLoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
@@ -74,6 +75,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("user")
                 .password(passwordEncoder().encode("qwer")).roles("USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
 
     @Bean
